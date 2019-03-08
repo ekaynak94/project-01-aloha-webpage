c541a1185c7eb16ccad976e1807a3b8efb556178
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-08 11:00:42 -0800

    Change variable types to let

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(function() {
 //  smooth scrolling source @ https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var target = $(this.hash);
+    let target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
     if (target.length) {
         $('html, body').animate({
@@ -51,7 +51,7 @@ $('.featured-list').flickity({
 
 // email validity checker source @https://stackoverflow.com/a/2507043
 function checkEmailValidity(email){
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    let regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
 }
 
